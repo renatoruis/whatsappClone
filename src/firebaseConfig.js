3e639a7b23f2f30fc6c599a30bc9bc4a70bacf9e
Renato Ruis - TIM <renatoruis@gmail.com>
2021-02-20 22:31:04 -0300

    ajustes pipeline environments

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,11 @@
-export default {
-  apiKey: process.env.APIKEY,
+const APIKEY = process.env.REACT_APP_APIKEY
+
+const firebaseConfig = {
+  apiKey: APIKEY,
   authDomain: "whatsappclone-lab.firebaseapp.com",
   projectId: "whatsappclone-lab",
   storageBucket: "whatsappclone-lab.appspot.com",
   messagingSenderId: "288091732624",
   appId: "1:288091732624:web:dc8137f1eb350812afb615"
 };
+export default firebaseConfig
